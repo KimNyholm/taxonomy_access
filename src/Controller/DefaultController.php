dad9615c703820ae230867b4a7f94bb44aef0226
Kim Nyholm <mail@kimnyholm.com>
2016-03-31 13:15:59 +0000

    Admin role edit route re-introduced.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -89,44 +89,48 @@ static function taxonomy_access_delete_role_url($roleId) {
   return $url->toString();
 }
 
-function UserRoleList(){
-  $roles=$this->taxonomyAccessService->_taxonomy_access_user_roles();
-  $rows=array();
+function taxonomy_access_admin() {
+  $roles = $this->taxonomyAccessService->_taxonomy_access_user_roles();
+  $active_rids = db_query(
+    'SELECT rid FROM {taxonomy_access_default} WHERE vid = :vid',
+    array(':vid' => TAXONOMY_ACCESS_GLOBAL_DEFAULT)
+  )->fetchCol();
+
+  $header = array(t('Role'), t('Status'), t('Operations'));
+  $rows = array();
+
   foreach ($roles as $rid => $role) {
-    $urlParameters=array('rid' => $rid);
-    $url=Url::fromRoute('taxonomy_access.settings_role', $urlParameters);
-    $link = \Drupal::l(t('Configure'), $url);
-    $roleEnabled = $this->taxonomyAccessService->taxonomy_access_role_enabled($rid);
-    $state = $roleEnabled ? t('Enabled') : t('Disabled') ;
-    $row = array($role->label(), $state, $link,);
-    $rows[]=$row;
-  }
-  return $rows  ;
-}
+    $row = array();
+    $row[] = $role->label();
 
-  public function taxonomy_access_admin() {
+    if (in_array($rid, $active_rids)) {
+      // Add edit operation link for active roles.
+      $row[] = array('data' => t('Enabled'));
 
-    $header = [t('Role'), t('Status'), t('Operations')];
-    $rows=$this->UserRoleList();
-
-    $build['role_table'] = [
-      '#theme' => 'table',
-      '#header' => $header,
-      '#rows' => $rows,
-    ];
-
-    return $build;
-  }
-
-  public function taxonomy_access_disable_vocab_confirm_page($rid, $vocab) {
-    $rid = intval($rid);
-
-    // Return a 404 on invalid vid or rid.
-    if (!$vocab->vid || !$rid) {
-      return MENU_NOT_FOUND;
     }
-
-    return \Drupal::formBuilder()->getForm('taxonomy_access_disable_vocab_confirm', $rid, $vocab);
+    else {
+      // Add enable link for unconfigured roles.
+      $row[] = array('data' => t('Disabled'));
+    }
+    $urlParameters=array('rid' => $rid);
+    $url=Url::fromRoute('taxonomy_access.admin_role_edit', $urlParameters);
+// FIX ME, configure missing settings symbol.
+    $row[]=\Drupal::l(t('Configure'), $url);
+/*    $row[] = array('data' => l(
+      t("Configure"),
+      TAXONOMY_ACCESS_CONFIG . "/role/$rid/edit",
+      array('attributes' => array('class' => array('module-link', 'module-link-configure')))));
+*/
+    $rows[] = $row;
   }
+
+  $build['role_table'] = array(
+    '#theme' => 'table',
+    '#header' => $header,
+    '#rows' => $rows,
+  );
+
+  return $build;
+}
 
 }
